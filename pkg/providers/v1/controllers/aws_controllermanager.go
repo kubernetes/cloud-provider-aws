2a948e2ebaed836e3b68eb85223ee8e6b88807d2
Nguyen Dinh <92940366+nguyenkndinh@users.noreply.github.com>
2022-02-22 19:04:44 -0800

    Add tagging controller configuration

---
+++ pkg/providers/v1/controllers/aws_controllermanager.go
@@ -0,0 +1,50 @@
+package controllers
+
+import (
+	"context"
+	"k8s.io/klog/v2"
+
+	cloudprovider "k8s.io/cloud-provider"
+	"k8s.io/cloud-provider/app"
+	cloudcontrollerconfig "k8s.io/cloud-provider/app/config"
+	"k8s.io/controller-manager/controller"
+	genericcontrollermanager "k8s.io/controller-manager/app"
+	taggingcontroller "k8s.io/cloud-provider-aws/pkg/providers/v1/controllers/tagging"
+)
+
+// BuildControllerInitializers is used to add new controllers built in this package to
+// the existing list of controllers from cloud-provider
+func BuildControllerInitializers() map[string]app.ControllerInitFuncConstructor {
+	controllerInitializers := app.DefaultInitFuncConstructors
+
+	taggingControllerInitFuncConstrustor :=  app.ControllerInitFuncConstructor{
+		InitContext: app.ControllerInitContext{
+			ClientName: "tagging-controller",
+		},
+		Constructor: startTaggingControllerWrapper,
+	}
+
+	controllerInitializers["tagging"] = taggingControllerInitFuncConstrustor
+
+	return controllerInitializers
+}
+
+// StartCloudNodeControllerWrapper is used to take cloud cofig as input and start cloud node controller
+func startTaggingControllerWrapper(initContext app.ControllerInitContext, completedConfig *cloudcontrollerconfig.CompletedConfig, cloud cloudprovider.Interface) app.InitFunc {
+	return func(ctx context.Context, controllerContext genericcontrollermanager.ControllerContext) (controller.Interface, bool, error) {
+		return startTaggingController(ctx, initContext, completedConfig, cloud)
+	}
+}
+
+func startTaggingController(ctx context.Context, initContext app.ControllerInitContext, completedConfig *cloudcontrollerconfig.CompletedConfig, cloud cloudprovider.Interface) (controller.Interface, bool, error) {
+	// Start the CloudNodeController
+	taggingcontroller, err := taggingcontroller.NewTaggingController()
+	if err != nil {
+		klog.Warningf("failed to start tagging controller: %s", err)
+		return nil, false, nil
+	}
+
+	go taggingcontroller.Run(ctx.Done())
+
+	return nil, true, nil
+}
---
+++ pkg/providers/v1/controllers/tagging/tagging-controller.go
@@ -0,0 +1,43 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package tagging
+
+import (
+	"k8s.io/klog/v2"
+)
+
+// eksResourceTagPrefix is the prefix for tag to group resources that are used by eks
+// for a particular cluster, this tag is added to the existing tags.
+// Example: "Key1=Val1,aws:eks:cluster-name:my-cluster=Val2"
+const eksResourceTagPrefix = "aws:eks:cluster-name:"
+
+// TaggingController is the controller implementation for tagging cluster resources
+type TaggingController struct {
+}
+
+// NewTaggingController creates a NewTaggingController object
+func NewTaggingController() (*TaggingController, error) {
+	tc := &TaggingController{
+	}
+
+	return tc, nil
+}
+
+// Run will start the controller to tag resources attached to a cluster
+// at any point in time
+func (tc *TaggingController) Run(stopCh <-chan struct{}) {
+	klog.Infof("Starting the TaggingController, eksResourceTagPrefix is %s.", eksResourceTagPrefix)
+
+	<-stopCh
+}
