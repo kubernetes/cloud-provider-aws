208f52eeae627257cc57e8990ba2252526300f56
Nguyen Dinh <92940366+nguyenkndinh@users.noreply.github.com>
2022-03-14 19:38:32 -0700

    Using ListNode to get nodes entering and leaving the cluster.
    * Remove route controller again
    
    * Print out clusterName for new nodes
    
    * tag new nodes when it comes online
    
    * only process a node once
    
    * check taggNodes size
    
    * add debugging
    
    * use node name as key
    
    * delete k,v from taggedNodes if node no longer exists
    
    * log if delete is done
    
    * Get a list of nodes and tag them if havent
    
    * get instance IDs of the nodes that need tagging
    
    * use MapToAWSInstanceID instead
    
    * restored v1 aws
    
    * restored from master
    
    * tag instance with a random tag
    
    * add klog
    
    * tag and untag node resources
    
    * Prepare for pr
    
    * Initialize nodeMap

--- pkg/controllers/aws_controller_manager.go
+++ pkg/controllers/aws_controller_manager.go
@@ -1,3 +1,16 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package controllers
 
 import (
@@ -8,6 +21,7 @@ import (
 	cloudcontrollerconfig "k8s.io/cloud-provider/app/config"
 	genericcontrollermanager "k8s.io/controller-manager/app"
 	"k8s.io/controller-manager/controller"
+	"k8s.io/klog/v2"
 )
 
 const (
@@ -29,6 +43,9 @@ func BuildControllerInitializers() map[string]app.ControllerInitFuncConstructor 
 
 	controllerInitializers[TaggingControllerKey] = taggingControllerConstructor
 
+	// TODO: remove the following line to enable the route controller
+	delete(controllerInitializers, "route")
+
 	return controllerInitializers
 }
 
@@ -41,13 +58,18 @@ func startTaggingControllerWrapper(initContext app.ControllerInitContext, comple
 
 func startTaggingController(ctx context.Context, initContext app.ControllerInitContext, completedConfig *cloudcontrollerconfig.CompletedConfig, cloud cloudprovider.Interface) (controller.Interface, bool, error) {
 	// Start the TaggingController
-	taggingcontroller, err := taggingcontroller.NewTaggingController()
+	taggingcontroller, err := taggingcontroller.NewTaggingController(
+		completedConfig.SharedInformers.Core().V1().Nodes(),
+		// cloud node lifecycle controller uses existing cluster role from node-controller
+		completedConfig.ClientBuilder.ClientOrDie(initContext.ClientName),
+		cloud,
+		completedConfig.ComponentConfig.KubeCloudShared.NodeMonitorPeriod.Duration)
 	if err != nil {
 		klog.Warningf("failed to start tagging controller: %s", err)
 		return nil, false, nil
 	}
 
-	go taggingcontroller.Run(ctx.Done())
+	go taggingcontroller.Run(ctx)
 
 	return nil, true, nil
 }
