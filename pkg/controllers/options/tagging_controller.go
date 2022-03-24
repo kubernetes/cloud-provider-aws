44da07c7afe0520d66fd9916297856861bc20db2
Nguyen Dinh <92940366+nguyenkndinh@users.noreply.github.com>
2022-03-24 13:37:52 -0700

    adding details for --resources
    * get user input for resources
    
    * Add better testing for failures to add flags
    
    * fix a small issue with --resources
    
    * finalize the --resources

--- pkg/controllers/options/tagging_controller.go
+++ pkg/controllers/options/tagging_controller.go
@@ -1,3 +1,16 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package options
 
 import (
@@ -6,16 +19,28 @@ import (
 )
 
 type TaggingControllerOptions struct {
-	Tags map[string]string
+	Tags      map[string]string
+	Resources []string
 }
 
 func (o *TaggingControllerOptions) AddFlags(fs *pflag.FlagSet) {
 	fs.StringToStringVar(&o.Tags, "tags", o.Tags, "Tags to apply to AWS resources in the tagging controller.")
+	fs.StringArrayVar(&o.Resources, "resources", o.Resources, "AWS resources name to add/remove tags in the tagging controller.")
 }
 
 func (o *TaggingControllerOptions) Validate() error {
 	if len(o.Tags) == 0 {
 		return fmt.Errorf("--tags must not be empty")
+	}
+
+	if len(o.Resources) == 0 {
+		return fmt.Errorf("--resources must not be empty")
+	}
+
+	for _, r := range o.Resources {
+		if _, ok := SupportedResources[r]; !ok {
+			return fmt.Errorf("%s is not a supported resource", r)
+		}
 	}
 
 	return nil
