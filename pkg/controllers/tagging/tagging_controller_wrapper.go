0016316081fee1a5bc4cd45823ece4cb0e0d2ddf
Saurav Agarwalla <sauravag@amazon.com>
2022-05-11 23:06:34 +0000

    Changes to make tagging controller compatible with K8s 1.20

--- pkg/controllers/tagging/tagging_controller_wrapper.go
+++ pkg/controllers/tagging/tagging_controller_wrapper.go
@@ -1,13 +1,12 @@
 package tagging
 
 import (
-	"context"
+	"net/http"
 
 	cloudprovider "k8s.io/cloud-provider"
 	"k8s.io/cloud-provider/app"
 	cloudcontrollerconfig "k8s.io/cloud-provider/app/config"
 	genericcontrollermanager "k8s.io/controller-manager/app"
-	"k8s.io/controller-manager/controller"
 	"k8s.io/klog/v2"
 
 	"k8s.io/cloud-provider-aws/pkg/controllers/options"
@@ -27,13 +26,13 @@ type ControllerWrapper struct {
 }
 
 // StartTaggingControllerWrapper is used to take cloud config as input and start the tagging controller
-func (tc *ControllerWrapper) StartTaggingControllerWrapper(initContext app.ControllerInitContext, completedConfig *cloudcontrollerconfig.CompletedConfig, cloud cloudprovider.Interface) app.InitFunc {
-	return func(ctx context.Context, controllerContext genericcontrollermanager.ControllerContext) (controller.Interface, bool, error) {
-		return tc.startTaggingController(ctx, initContext, completedConfig, cloud)
+func (tc *ControllerWrapper) StartTaggingControllerWrapper(completedConfig *cloudcontrollerconfig.CompletedConfig, cloud cloudprovider.Interface) app.InitFunc {
+	return func(ctx genericcontrollermanager.ControllerContext) (http.Handler, bool, error) {
+		return tc.startTaggingController(ctx, completedConfig, cloud)
 	}
 }
 
-func (tc *ControllerWrapper) startTaggingController(ctx context.Context, initContext app.ControllerInitContext, completedConfig *cloudcontrollerconfig.CompletedConfig, cloud cloudprovider.Interface) (controller.Interface, bool, error) {
+func (tc *ControllerWrapper) startTaggingController(ctx genericcontrollermanager.ControllerContext, completedConfig *cloudcontrollerconfig.CompletedConfig, cloud cloudprovider.Interface) (http.Handler, bool, error) {
 	err := tc.Options.Validate()
 	if err != nil {
 		klog.Fatalf("Tagging controller inputs are not properly set: %v", err)
@@ -42,7 +41,7 @@ func (tc *ControllerWrapper) startTaggingController(ctx context.Context, initCon
 	// Start the Controller
 	taggingcontroller, err := NewTaggingController(
 		completedConfig.SharedInformers.Core().V1().Nodes(),
-		completedConfig.ClientBuilder.ClientOrDie(initContext.ClientName),
+		completedConfig.ClientBuilder.ClientOrDie(TaggingControllerClientName),
 		cloud,
 		completedConfig.ComponentConfig.KubeCloudShared.NodeMonitorPeriod.Duration,
 		tc.Options.Tags,
@@ -53,7 +52,7 @@ func (tc *ControllerWrapper) startTaggingController(ctx context.Context, initCon
 		return nil, false, nil
 	}
 
-	go taggingcontroller.Run(ctx.Done())
+	go taggingcontroller.Run(ctx.Stop)
 
 	return nil, true, nil
 }
