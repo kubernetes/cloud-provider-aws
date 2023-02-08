69bbab3043404278099d01731d7839d2e15cdd47
Konstantinos Angelopoulos <konstantinos.angelopoulos@sap.com>
2023-02-08 13:06:35 +0100

    fix/ add new subnet to LB on UpdateLoadbalancers

--- pkg/providers/v1/aws.go
+++ pkg/providers/v1/aws.go
@@ -628,29 +628,29 @@ type CloudConfig struct {
 		// KubernetesClusterID is the cluster id we'll use to identify our cluster resources
 		KubernetesClusterID string
 
-		//The aws provider creates an inbound rule per load balancer on the node security
-		//group. However, this can run into the AWS security group rule limit of 50 if
-		//many LoadBalancers are created.
+		// The aws provider creates an inbound rule per load balancer on the node security
+		// group. However, this can run into the AWS security group rule limit of 50 if
+		// many LoadBalancers are created.
 		//
-		//This flag disables the automatic ingress creation. It requires that the user
-		//has setup a rule that allows inbound traffic on kubelet ports from the
-		//local VPC subnet (so load balancers can access it). E.g. 10.82.0.0/16 30000-32000.
+		// This flag disables the automatic ingress creation. It requires that the user
+		// has setup a rule that allows inbound traffic on kubelet ports from the
+		// local VPC subnet (so load balancers can access it). E.g. 10.82.0.0/16 30000-32000.
 		DisableSecurityGroupIngress bool
 
-		//AWS has a hard limit of 500 security groups. For large clusters creating a security group for each ELB
-		//can cause the max number of security groups to be reached. If this is set instead of creating a new
-		//Security group for each ELB this security group will be used instead.
+		// AWS has a hard limit of 500 security groups. For large clusters creating a security group for each ELB
+		// can cause the max number of security groups to be reached. If this is set instead of creating a new
+		// Security group for each ELB this security group will be used instead.
 		ElbSecurityGroup string
 
-		//During the instantiation of an new AWS cloud provider, the detected region
-		//is validated against a known set of regions.
+		// During the instantiation of an new AWS cloud provider, the detected region
+		// is validated against a known set of regions.
 		//
-		//In a non-standard, AWS like environment (e.g. Eucalyptus), this check may
-		//be undesirable.  Setting this to true will disable the check and provide
-		//a warning that the check was skipped.  Please note that this is an
-		//experimental feature and work-in-progress for the moment.  If you find
-		//yourself in an non-AWS cloud and open an issue, please indicate that in the
-		//issue body.
+		// In a non-standard, AWS like environment (e.g. Eucalyptus), this check may
+		// be undesirable.  Setting this to true will disable the check and provide
+		// a warning that the check was skipped.  Please note that this is an
+		// experimental feature and work-in-progress for the moment.  If you find
+		// yourself in an non-AWS cloud and open an issue, please indicate that in the
+		// issue body.
 		DisableStrictZoneCheck bool
 
 		// NodeIPFamilies determines which IP addresses are added to node objects and their ordering.
@@ -4825,7 +4825,7 @@ func (c *Cloud) EnsureLoadBalancerDeleted(ctx context.Context, clusterName strin
 			sgID := aws.StringValue(sg.GroupId)
 
 			if sgID == c.cfg.Global.ElbSecurityGroup {
-				//We don't want to delete a security group that was defined in the Cloud Configuration.
+				// We don't want to delete a security group that was defined in the Cloud Configuration.
 				continue
 			}
 			if sgID == "" {
@@ -4934,6 +4934,24 @@ func (c *Cloud) UpdateLoadBalancer(ctx context.Context, clusterName string, serv
 				return err
 			}
 		}
+	}
+
+	shouldEnsureLoadBalancer := func() bool {
+		subnets := sets.NewString()
+		for _, sub := range lb.Subnets {
+			subnets.Insert(*(sub))
+		}
+		for _, instance := range instances {
+			if instance.SubnetId != nil && !subnets.Has(*instance.SubnetId) {
+				return true
+			}
+		}
+		return false
+	}()
+
+	if shouldEnsureLoadBalancer {
+		_, err := c.EnsureLoadBalancer(ctx, clusterName, service, nodes)
+		return err
 	}
 
 	err = c.ensureLoadBalancerInstances(aws.StringValue(lb.LoadBalancerName), lb.Instances, instances)
