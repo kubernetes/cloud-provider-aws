71d02ba22df7f4f3a7be4d0414de0cc570b1bd6e
Carter McKinnon <mckdev@amazon.com>
2023-11-27 21:12:19 +0000

    Drop Node events when EC2 instance does not exist and node is not new

--- pkg/providers/v1/aws.go
+++ pkg/providers/v1/aws.go
@@ -1768,7 +1768,7 @@ func (c *Cloud) InstanceExistsByProviderID(ctx context.Context, providerID strin
 	instances, err := c.ec2.DescribeInstances(request)
 	if err != nil {
 		// if err is InstanceNotFound, return false with no error
-		if isAWSErrorInstanceNotFound(err) {
+		if IsAWSErrorInstanceNotFound(err) {
 			return false, nil
 		}
 		return false, err
@@ -2007,7 +2007,8 @@ func (c *Cloud) GetZoneByNodeName(ctx context.Context, nodeName types.NodeName) 
 
 }
 
-func isAWSErrorInstanceNotFound(err error) bool {
+// IsAWSErrorInstanceNotFound returns true if the specified error is an awserr.Error with the code `InvalidInstanceId.NotFound`.
+func IsAWSErrorInstanceNotFound(err error) bool {
 	if err == nil {
 		return false
 	}
