ef820bf15ad0251521935173eed83b317a02a272
Matt Merkes <merkes@amazon.com>
2024-10-28 11:52:15 -0700

    Migrates InstanceTopologyManager to use sdk v2

--- go.mod
+++ go.mod
@@ -35,6 +35,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.21 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.21 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ec2 v1.186.0
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.24.2 // indirect
--- pkg/resourcemanagers/topology.go
+++ pkg/resourcemanagers/topology.go
@@ -14,16 +14,19 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package aws
+package resourcemanagers
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/aws/smithy-go"
 	"k8s.io/client-go/tools/cache"
-	"k8s.io/cloud-provider-aws/pkg/providers/v1/iface"
+	"k8s.io/cloud-provider-aws/pkg/services"
 	"k8s.io/klog/v2"
 )
 
@@ -40,12 +43,19 @@ func topStringKeyFunc(obj interface{}) (string, error) {
 	return s, nil
 }
 
+// InstanceTopologyManager enables mocking the InstanceTopologyManager.
+type InstanceTopologyManager interface {
+	GetNodeTopology(ctx context.Context, instanceType string, region string, instanceID string) (*types.InstanceTopology, error)
+}
+
+// instanceTopologyManager manages getting instance topology for nodes.
 type instanceTopologyManager struct {
-	ec2                 iface.EC2
+	ec2                 services.Ec2SdkV2
 	unsupportedKeyStore cache.Store
 }
 
-func newInstanceTopologyManager(ec2 iface.EC2) *instanceTopologyManager {
+// NewInstanceTopologyManager generates a new InstanceTopologyManager.
+func NewInstanceTopologyManager(ec2 services.Ec2SdkV2) InstanceTopologyManager {
 	return &instanceTopologyManager{
 		ec2: ec2,
 		// These should change very infrequently, if ever, so checking once a day sounds fair.
@@ -53,13 +63,16 @@ func newInstanceTopologyManager(ec2 iface.EC2) *instanceTopologyManager {
 	}
 }
 
-func (t *instanceTopologyManager) getNodeTopology(instanceType string, region string, instanceID string) (*ec2.InstanceTopology, error) {
+// GetNodeTopology gets the instance topology for a node.
+func (t *instanceTopologyManager) GetNodeTopology(ctx context.Context, instanceType string, region string, instanceID string) (*types.InstanceTopology, error) {
 	if t.mightSupportTopology(instanceType, region) {
-		request := &ec2.DescribeInstanceTopologyInput{InstanceIds: []*string{&instanceID}}
-		topologies, err := t.ec2.DescribeInstanceTopology(request)
+		request := &ec2.DescribeInstanceTopologyInput{InstanceIds: []string{instanceID}}
+		topologies, err := t.ec2.DescribeInstanceTopology(ctx, request)
 		if err != nil {
-			if aerr, ok := err.(awserr.Error); ok {
-				switch aerr.Code() {
+			var apiErr smithy.APIError
+			if errors.As(err, &apiErr) {
+				code := apiErr.ErrorCode()
+				switch code {
 				case "UnsupportedOperation":
 					klog.Infof("ec2:DescribeInstanceTopology is not available in %s: %q", region, err)
 					// If region is unsupported, track it to avoid making the call in the future.
@@ -86,7 +99,7 @@ func (t *instanceTopologyManager) getNodeTopology(instanceType string, region st
 			return nil, nil
 		}
 
-		return topologies[0], nil
+		return &topologies[0], nil
 	}
 	return nil, nil
 }
